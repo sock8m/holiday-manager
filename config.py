28f0339f1f95b034f9cb4cdc1b120dd439fdc33b
Yiqing Guo <yguoj80@gmail.com>
2022-07-11 05:27:29 -0400

    added plans

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ currentYear = 2022
 city = "New York City, USA"
 weatherAPIKey = "55e9611df7msh8d9a1794a95dd96p1ff35bjsn79b2ab7fe896"
 starterjsonfileloc = 'holidays.json'
-newfilelocation = "test_holiday.json"
+newfilelocation = "output_holiday.json"
 timeanddateURL = "https://www.timeanddate.com/holidays/us/{}?hol=33554809"
 openweatherAPIURL = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
